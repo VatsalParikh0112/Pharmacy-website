58dbe8acc75c0f4e6783e53100070c1ce5f6f93a
VatsalParikh0112 <vatsalparikh0112@gmail.com>
2025-03-17 02:24:03 +0530

    solving error 3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Pharmacy-website/", // Use the project folder as base
+  base: "/", // Use the project folder as base
   plugins: [react()],
 });
